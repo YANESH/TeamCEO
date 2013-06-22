476986f505f442d3bc6c753d84d76f1de9163602
Yanesh Prithipaul <yanesh1@gmail.com>
2013-06-22 01:37:51 -0400

    MAP: centered on RBC

---
+++ js.js
@@ -0,0 +1,14 @@
+function initialize() {
+var mapOptions = {
+
+	//center = 315+Front+Street+West,+Toronto+ON+M5V+3A4
+  center: new google.maps.LatLng(43.64350550, -79.38945280),
+  zoom: 16,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map-canvas"),
+	mapOptions);
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
